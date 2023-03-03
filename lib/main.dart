42268a7e43bbbef58a4bbb9960514eba338a28fc
Ismael Custódio Santana <79366286+IsmaelDevMobile@users.noreply.github.com>
2023-03-03 14:36:15 -0300

    adicionar formulário no modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
-import 'package:expenses/widgets/transacion_user.dart';
+import 'dart:math';
+
+import 'package:expenses/models/transaction.dart';
+import 'package:expenses/widgets/transaction_form.dart';
+import 'package:expenses/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const ExpensesApp());
@@ -15,8 +19,52 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+   _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transaction.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context, 
+    builder: (_){
+    return TransactionForm(
+      onSubmit: _addTransaction,
+    );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,26 +74,28 @@ class MyHomePage extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => _openTransactionFormModal(context),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Card(
+          children: [
+            const Card(
               color: Colors.blue,
               elevation: 5,
               child: Text('Gráfico'),
             ),
-            TransactionUser(),
+            TransactionList(
+          transactions: _transaction,
+        ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
