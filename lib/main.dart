903b879ffae8bcb9b1e9bfc8b32af4e0c0225563
Ismael Custódio Santana <79366286+IsmaelDevMobile@users.noreply.github.com>
2023-03-06 09:49:37 -0300

    adicionar navigator para fechar formulário modal

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transaction.add(newTransaction);
     });
+    
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
