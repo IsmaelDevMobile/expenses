8d0cd4fb8325da6b6cf8467718f6c16442642be7
Ismael Custódio Santana <79366286+IsmaelDevMobile@users.noreply.github.com>
2023-05-15 09:58:19 -0300

    adicionar chart widget #05

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -21,7 +21,30 @@ class ChartBar extends StatelessWidget {
         Container(
           height: 60,
           width: 10,
-          child: null,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(158, 158, 158, 1),
+                    width: 1.0,
+                  ),
+                  color: Color.fromARGB(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
         const SizedBox(height: 5),
         Text(label),
